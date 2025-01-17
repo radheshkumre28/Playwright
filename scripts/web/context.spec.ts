70fd029681ff17a96d192a5ca143f0715024ff06
radheshkumre28 <radheshkumre2899@gmail.com>
2025-01-17 17:56:25 +0530

    Radhesh: Context with assertion completed

--- scripts/web/context.spec.ts
+++ scripts/web/context.spec.ts
@@ -1,4 +1,4 @@
-import {test,expect,Browser, BrowserContext, Page} from '@playwright/test'
+import {test,expect,Browser, BrowserContext, Page, Locator} from '@playwright/test'
 import { chromium, firefox, webkit } from '@playwright/test'
 import { beforeEach } from 'node:test';
 
@@ -8,6 +8,8 @@ test.describe('create context with chrome', async()=>{
     let pageOne : Page;
     let browserOne : Browser;
     let browserContextOne : BrowserContext;
+
+
   
 
     test.beforeAll(async()=>{
@@ -26,6 +28,22 @@ test.describe('create context with chrome', async()=>{
 
     test('Context one launch with chrome', async()=>{
         await pageOne.goto('https://www.automationexercise.com/login')
+    })
+
+    test('Chrom Login', async()=>{
+
+        const emailID : Locator = await pageOne.locator('[data-qa="login-email"]');
+        const password : Locator = await pageOne.locator('[data-qa="login-password"]');
+        const submitButton : Locator = await pageOne.locator('[data-qa="login-button"]')
+
+        await emailID.fill('radheshkumre2899@gmail.com');
+        await password.fill('Kumre');
+        await submitButton.click();
+
+        await expect(pageOne.locator('b')).toHaveText('Radhesh Kumre')
+
+        // await pageOne.pause()
+
     })
 
 
@@ -53,7 +71,24 @@ test.describe('Context run with webkit',async()=>{
         await browserContextTwo.close();
         await browserTwo.close();
     })
+
     test('Context two launch with webkit',async()=>{
         await pageTwo.goto('https://www.automationexercise.com/login')
+    })
+
+    test('Webkit login', async()=>{
+        const emailID : Locator = await pageTwo.locator('[data-qa="login-email"]');
+        const password : Locator = await pageTwo.locator('[data-qa="login-password"]');
+        const submitButton : Locator = await pageTwo.locator('[data-qa="login-button"]')
+
+        await emailID.fill('testradhesh@gmail.com');
+        await password.fill('test');
+        await submitButton.click();
+
+        await expect(pageTwo.locator('b')).toHaveText('Radhesh Test')
+
+        // await pageTwo.pause()
+
+
     })
 });
