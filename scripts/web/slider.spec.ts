f1b88077e187067850de08061e36632622f6d8f8
radheshkumre28 <radheshkumre2899@gmail.com>
2025-02-05 12:56:54 +0530

    Radhesh: Slider using boundingbox and clicks

--- scripts/web/slider.spec.ts
+++ scripts/web/slider.spec.ts
@@ -1,6 +1,7 @@
 import {test,expect,Browser, BrowserContext, Page, Locator} from '@playwright/test'
 import { chromium,webkit } from '@playwright/test'
 
+type BoundingBox = {x : number, y : number, width : number, height : number}
 
 test.describe('Perform Slider bar', ()=>{
     let page : Page;
@@ -27,16 +28,16 @@ test.describe('Perform Slider bar', ()=>{
         await expect(slider).toBeVisible()
 
         //get silder's bounding box i.e its position and size
-        const sliderBox  = slider.boundingBox()
+        const sliderBox  = await slider.boundingBox()
 
 
-        const {x, y, width,heigth} = sliderBox;
+        const { x , y , width, height } =  sliderBox as BoundingBox;
 
         const clickPositions = [0.1, 0.3, 0.6, 0.9];
 
         for (const position of clickPositions){
             const clickX = x + position * width
-            const clickY = y + heigth / 2
+            const clickY = y + height / 2
 
             await page.mouse.click(clickX,clickY);
             await page.waitForTimeout(500);
